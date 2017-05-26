3887ef935bb7f0ca5e0ceb74591652e437494788
Chie Akiba <akiba.chie@gmail.com>
2017-05-26 15:11:10 -0700

    Fixed the edit button controller and made the code more DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,18 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def index
     @articles = Article.all
   end
+
   def show
       @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
   end
 
   def create
